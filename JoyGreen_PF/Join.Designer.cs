79b42609218036f8296253eb7c5c2c2a182e0c6f
secretaistudy <assassinaideveloper@gmail.com>
2023-12-10 21:01:59 +0900

    Create Join.Designer.cs

---
+++ JoyGreen_PF/Join.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace JoyGreen_PF
+{
+    partial class Join
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Join));
+            this.joinName = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.joinPwd = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.join_btn = new System.Windows.Forms.Button();
+            this.joinID = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.loginlabel = new System.Windows.Forms.Label();
+            this.read_pw = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // joinName
+            // 
+            this.joinName.Font = new System.Drawing.Font("굴림", 16F);
+            this.joinName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(193)))), ((int)(((byte)(194)))));
+            this.joinName.Location = new System.Drawing.Point(460, 513);
+            this.joinName.Margin = new System.Windows.Forms.Padding(0);
+            this.joinName.MaxLength = 20;
+            this.joinName.Name = "joinName";
+            this.joinName.Size = new System.Drawing.Size(262, 56);
+            this.joinName.TabIndex = 54;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("굴림", 18F);
+            this.label6.Location = new System.Drawing.Point(203, 519);
+            this.label6.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(116, 48);
+            this.label6.TabIndex = 64;
+            this.label6.Text = "이름";
+            // 
+            // joinPwd
+            // 
+            this.joinPwd.Font = new System.Drawing.Font("굴림", 16F);
+            this.joinPwd.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(193)))), ((int)(((byte)(194)))));
+            this.joinPwd.Location = new System.Drawing.Point(460, 417);
+            this.joinPwd.Margin = new System.Windows.Forms.Padding(0);
+            this.joinPwd.MaxLength = 20;
+            this.joinPwd.Name = "joinPwd";
+            this.joinPwd.PasswordChar = '*';
+            this.joinPwd.Size = new System.Drawing.Size(622, 56);
+            this.joinPwd.TabIndex = 53;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 18F);
+            this.label1.Location = new System.Drawing.Point(203, 433);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(212, 48);
+            this.label1.TabIndex = 63;
+            this.label1.Text = "비밀번호";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(97, 606);
+            this.label8.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(250, 24);
+            this.label8.TabIndex = 62;
+            this.label8.Text = "     로그인을 해주세요";
+            // 
+            // label7
+            // 
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label7.Location = new System.Drawing.Point(101, 636);
+            this.label7.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(1005, 20);
+            this.label7.TabIndex = 61;
+            // 
+            // join_btn
+            // 
+            this.join_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(161)))), ((int)(((byte)(255)))));
+            this.join_btn.Font = new System.Drawing.Font("굴림", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.join_btn.ForeColor = System.Drawing.Color.White;
+            this.join_btn.Location = new System.Drawing.Point(772, 513);
+            this.join_btn.Margin = new System.Windows.Forms.Padding(6);
+            this.join_btn.Name = "join_btn";
+            this.join_btn.Size = new System.Drawing.Size(314, 64);
+            this.join_btn.TabIndex = 55;
+            this.join_btn.Text = "회원가입";
+            this.join_btn.UseVisualStyleBackColor = false;
+            this.join_btn.Click += new System.EventHandler(this.join_btn_Click);
+            // 
+            // joinID
+            // 
+            this.joinID.Font = new System.Drawing.Font("굴림", 16F);
+            this.joinID.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(193)))), ((int)(((byte)(194)))));
+            this.joinID.Location = new System.Drawing.Point(460, 327);
+            this.joinID.Margin = new System.Windows.Forms.Padding(0);
+            this.joinID.MaxLength = 20;
+            this.joinID.Name = "joinID";
+            this.joinID.Size = new System.Drawing.Size(622, 56);
+            this.joinID.TabIndex = 52;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("굴림", 18F);
+            this.label5.Location = new System.Drawing.Point(203, 343);
+            this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(164, 48);
+            this.label5.TabIndex = 59;
+            this.label5.Text = "아이디";
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Location = new System.Drawing.Point(101, 237);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(984, 20);
+            this.label4.TabIndex = 58;
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Location = new System.Drawing.Point(101, 221);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(984, 20);
+            this.label3.TabIndex = 57;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("굴림", 17F);
+            this.label2.Location = new System.Drawing.Point(981, 167);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 46);
+            this.label2.TabIndex = 60;
+            this.label2.Text = "닫기";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("굴림", 38F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+            this.label10.Location = new System.Drawing.Point(94, 136);
+            this.label10.Margin = new System.Windows.Forms.Padding(0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(173, 38);
+            this.label10.TabIndex = 66;
+            this.label10.Text = "회원가입";
+            // 
+            // loginlabel
+            // 
+            this.loginlabel.AutoSize = true;
+            this.loginlabel.Font = new System.Drawing.Font("굴림", 38F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+            this.loginlabel.Location = new System.Drawing.Point(972, 61);
+            this.loginlabel.Margin = new System.Windows.Forms.Padding(0);
+            this.loginlabel.Name = "loginlabel";
+            this.loginlabel.Size = new System.Drawing.Size(134, 38);
+            this.loginlabel.TabIndex = 67;
+            this.loginlabel.Text = "로그인";
+            this.loginlabel.Click += new System.EventHandler(this.loginlabel_Click);
+            // 
+            // read_pw
+            // 
+            this.read_pw.AutoSize = true;
+            this.read_pw.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.read_pw.Location = new System.Drawing.Point(1104, 429);
+            this.read_pw.Margin = new System.Windows.Forms.Padding(4);
+            this.read_pw.Name = "read_pw";
+            this.read_pw.Size = new System.Drawing.Size(281, 36);
+            this.read_pw.TabIndex = 68;
+            this.read_pw.Text = "비밀번호 보이기";
+            this.read_pw.UseVisualStyleBackColor = true;
+            this.read_pw.CheckedChanged += new System.EventHandler(this.read_pw_CheckedChanged);
+            // 
+            // Join
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1543, 933);
+            this.Controls.Add(this.read_pw);
+            this.Controls.Add(this.loginlabel);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.joinName);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.joinPwd);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.join_btn);
+            this.Controls.Add(this.joinID);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Join";
+            this.Text = "Join";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox joinName;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox joinPwd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button join_btn;
+        private System.Windows.Forms.TextBox joinID;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label loginlabel;
+        private System.Windows.Forms.CheckBox read_pw;
+    }
+}
